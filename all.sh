7eca4564aa9d14760ea97baa7fc14c47508c0774
Atsushi Watanabe <atsushi.w@ieee.org>
2018-07-19 17:49:52 +0900

    Resolve dependency tree

--- all.sh
+++ all.sh
@@ -26,12 +26,74 @@ done > /tmp/building
 
 echo "----------------"
 echo "building:"
-cat /tmp/building
+cat /tmp/building | sed "s/^/- /"
 echo "----------------"
 
 sudo apk update
 
+echo "----------------"
+echo "checking deps:"
+rm -rf /tmp/deps
+mkdir -p /tmp/deps
+rm -rf /tmp/subs
+mkdir -p /tmp/subs
 cat /tmp/building | while read pkg
+do
+  touch /tmp/deps/$pkg
+  touch /tmp/subs/$pkg
+  (source $pkg/APKBUILD && echo $makedepends) | xargs -r -n1 echo | while read dep
+  do
+    apk info $dep > /dev/null || echo $dep >> /tmp/deps/$pkg
+  done
+  (source $pkg/APKBUILD && echo $subpackages) | xargs -r -n1 echo | while read sub
+  do
+    echo $sub >> /tmp/subs/$pkg
+  done
+  echo "  $pkg requires:"
+  cat /tmp/deps/$pkg | sed "s/^/  - /"
+done
+echo "----------------"
+
+echo "----------------"
+echo "generating build tree:"
+rm -f /tmp/building2
+touch /tmp/building2
+while true
+do
+  nremain=`cat /tmp/building | sed '/^\s*$/d' | wc -l`
+  if [ $nremain -eq 0 ]
+  then
+    break
+  fi
+
+  newresolve=false
+  while read pkg
+  do
+    ndep=`cat /tmp/deps/$pkg | sed '/^\s*$/d' | wc -l`
+    if [ $ndep -eq 0 ]
+    then
+      echo "- $pkg"
+      echo $pkg >> /tmp/building2
+      newresolve=true
+      rm /tmp/deps/$pkg
+      (ls -1 /tmp/deps/* 2> /dev/null || true) | xargs -r -n1 sed -e "/^$pkg$/d" -i
+      while read sub
+      do
+        (ls -1 /tmp/deps/* 2> /dev/null || true) | xargs -r -n1 sed -e "/^$sub$/d" -i
+      done < /tmp/subs/$pkg
+      sed -e "/^$pkg$/d" -i /tmp/building
+    fi
+  done < /tmp/building
+
+  if [ $newresolve == "false" ]
+  then
+    echo "Failed to resolve dependency tree for:"
+    cat /tmp/building | sed "s/^/- /"
+  fi
+done
+echo "----------------"
+
+cat /tmp/building2 | while read pkg
 do
   echo "----------------"
   exist=true
