f1f919c40d93aa72011d9ce9b6a100787f2b2ec9
f-fl0 <florent@seqsense.com>
2021-11-04 18:40:49 +0900

    Add a step to validate existing .rosinstall files

--- build-repo.sh
+++ build-repo.sh
@@ -133,6 +133,11 @@ fi
 
 ext_deps=$(find ${SRCDIR} -name "*.rosinstall" || true)
 for dep in ${ext_deps}; do
+  vcs validate --input ${dep}
+  if [ ! $? -eq 0 ]; then
+    echo "Skipping ${dep}: no valid repository found."
+    continue
+  fi
   vcs import --input ${dep} ${ext_pkg_option} ${VCS_OPTIONS} --workers 4 ${extsrc}
 done
 
