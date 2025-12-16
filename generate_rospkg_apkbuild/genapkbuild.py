1cee26a2069437c3033f0becc7a970fd2901c869
Atsushi Watanabe <atsushi.w@ieee.org>
2025-12-16 22:08:31 +0900

    Fix variable

--- generate_rospkg_apkbuild/genapkbuild.py
+++ generate_rospkg_apkbuild/genapkbuild.py
@@ -189,7 +189,7 @@ def package_to_apkbuild(ros_distro, package_name,
     if alpine_version_major == 3 and alpine_version_minor >= 23:
         # Specify rpath of shared objects to make abuild possible to find library dependencies under /usr/ros
         # TODO(at-wat): Make this default once Alpine ROS 3.20 is dropped
-        cmake_vars.insert(0, f'-DCMAKE_INSTALL_RPATH=/usr/ros/{ros_distro}/lib')
+        cmake_vars.insert(0, f'CMAKE_INSTALL_RPATH=/usr/ros/{ros_distro}/lib')
 
     if package_name.startswith('http://') or package_name.startswith('https://'):
         import requests
